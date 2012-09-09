be00c982598fbae38b8903f37c925ebc8f3e93c8
Aaron Hall <aaronchall@yahoo.com>
2012-09-09 19:10:59 -0400

    modified:   key_variables.yml 	modified:   model_loader.rb 	deleted:    .key_variables.yml.swp 	deleted:    ATCF.rb

--- model_loader.rb
+++ model_loader.rb
@@ -1,5 +1,13 @@
 $:.unshift File.dirname(__FILE__) # $: means $LOAD_PATH, an array, unshift prepends the new location
 Dir.glob(File.dirname(__FILE__) + '/helpers/*') {|file| require file} #from http://stackoverflow.com/questions/2488342/cleanest-one-liner-way-to-require-all-files-in-directory-in-ruby
-require 'psych'
+#require 'psych'
 #figure out how to replace --- with the yml file, code: Psych.load('--- foo') # => 'foo'
+require 'yaml'
 
+parsed = begin
+		   YAML.load(File.open("key_variables.yml"))
+	 rescue ArgumentError => e
+		   puts "Could not parse YAML: #{e.message}"
+	 end
+
+puts parsed
