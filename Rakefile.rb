275174d398ff8c4819a994ba7502a64a35a3f92c
Aaron Hall <aaronchall@yahoo.com>
2012-09-11 22:16:01 -0400

    new file:   .ATCF_from_installments.rb.swp 	modified:   Rakefile.rb

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,4 +1,8 @@
-directory "tmp"
-file "hello.tmp" => "tmp" do
+task :name => [:prereq1, :prereq2] do |t|
+	#actions that reference t (why?)
+end
+
+#directory "tmp"
+file "model_loader.rb" => "tmp" do
   sh "echo 'Hello' > 'tmp/hello.tmp'"
 end
